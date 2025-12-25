6e7600c5f0bc2181d54e8a1695b9db6540169a25
rscortgamerYT <royalshikher@gmail.com>
2025-12-25 16:09:33 +0530

    Clean workspace page: remove all modules for rebuilding

---
+++ apps/web/app/workspace/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function WorkspacePage() {
+    return (
+        <div className="min-h-screen bg-black">
+            {/* Empty workspace - ready for rebuilding */}
+        </div>
+    );
+}
